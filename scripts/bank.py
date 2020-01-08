aa15bb69e4b7a6e8655d5e81c95e6f96185da8f0
Data-drone <bpl.law@gmail.com>
2020-01-08 20:33:16 +1100

    added a little variation to data and also print so that nifi picks it up

--- scripts/bank.py
+++ scripts/bank.py
@@ -2,7 +2,7 @@
 # keywords / time / credit card / person
 
 from faker import Faker
-from numpy.random import Generator, PCG64
+from numpy.random import Generator, PCG64, binomial
 import datetime
 
 # ISO 8601 compact timestamp
@@ -15,7 +15,18 @@ card_id = fake.credit_card_full(card_type=None)
 
 # we will use some distributions so there is something statistically interesting
 value_rng = Generator(PCG64())
-value = value_rng.normal(mu=20, sigma=1, size=1) 
+value_low = value_rng.normal(mu=20, sigma=1, size=1) 
+value_high =  value_rng.normal(mu=1000, sigma=1, size=1)
+# lets mix things up a little and have two possible values
+
+binom_bit = binomial(1, 0.9)
+
+if binom_bit==1:
+    value = value_high
+else:
+    value = value_low
+
 
 return_tuple = {'card_identifier': card_id, 'timestamp': current_time, 'value':value}
 
+print(return_tuple)
