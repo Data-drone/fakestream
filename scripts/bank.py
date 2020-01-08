d0a50df594f0762e56e1874f2d7228eaf8faaab7
Data-drone <bpl.law@gmail.com>
2020-01-08 23:07:36 +1100

    fixed the numpy rngs

--- scripts/bank.py
+++ scripts/bank.py
@@ -2,8 +2,9 @@
 # keywords / time / credit card / person
 
 from faker import Faker
-from numpy.random import Generator, PCG64, binomial
+from numpy.random import default_rng, binomial
 import datetime
+import json
 
 # ISO 8601 compact timestamp
 current_time = datetime.datetime.utcnow().strftime("%Y%m%dT%H%M%S.%fZ")
@@ -14,14 +15,15 @@ fake = Faker()
 card_id = fake.credit_card_full(card_type=None)
 
 # we will use some distributions so there is something statistically interesting
-value_rng = Generator(PCG64())
-value_low = value_rng.normal(mu=20, sigma=1, size=1) 
-value_high =  value_rng.normal(mu=1000, sigma=1, size=1)
+mu, sigma = 20, 3
+value_low = default_rng().normal(mu, sigma, size=1)[0]
+mu, sigma = 1000, 100
+value_high =  default_rng().normal(mu, sigma, size=1)[0]
 # lets mix things up a little and have two possible values
 
 binom_bit = binomial(1, 0.9)
 
-if binom_bit==1:
+if binom_bit!=1:
     value = value_high
 else:
     value = value_low
@@ -29,4 +31,4 @@ else:
 
 return_tuple = {'card_identifier': card_id, 'timestamp': current_time, 'value':value}
 
-print(return_tuple)
+print(json.dumps(return_tuple))
