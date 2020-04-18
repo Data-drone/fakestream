e358ba05dd3b159023ee5534928dc5487bdd647e
Data-drone <bpl.law@gmail.com>
2020-04-18 23:28:06 +1000

    added sensorstream class and flink for testing

---
+++ scripts/generators/generator_sensorstream.py
@@ -0,0 +1,36 @@
+from .generator_framework import DataGenerator
+import datetime
+import json
+from argparse import ArgumentParser
+import numpy as np
+
+# industrial sensor data generator
+
+class SensorGenerator(DataGenerator):
+
+    """
+
+    testing using a full generator
+
+    """
+
+    def __init__(self, batch):
+
+        self.batch = batch
+        self.origin = np.zeros((1, 1))
+        self.step_set = [-1, 0, 1]
+
+    def emit(self) -> list:
+
+        while True:
+            step_shape = (self.batch, 1)
+            steps = np.random.choice(a = self.step_set, size=step_shape)
+            path = np.concatenate([self.origin, steps]).cumsum(0)
+            self.origin = np.zeros((1,1)) + path[-1:]
+
+            path = path[1:]
+            output = []
+            for i in range(len(path)):
+                output.append([datetime.utcnow(), path[i]])  
+
+            yield output
