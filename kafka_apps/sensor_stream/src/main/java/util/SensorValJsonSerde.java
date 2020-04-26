112f7d959556e963b42e9e6f7229ddcc50282e50
Data-drone <bpl.law@gmail.com>
2020-04-27 00:24:29 +1000

    added Serde class

--- kafka_apps/sensor_stream/src/main/java/util/SensorValJsonSerde.java
+++ kafka_apps/sensor_stream/src/main/java/util/SensorValJsonSerde.java
@@ -3,5 +3,56 @@ package util;
 import model.SensorVal;
 
 import org.apache.kafka.common.serialization.Deserializer;
-import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serializer;
+
+import com.google.gson.Gson;
+
+/*
+* Json Serde for SensorVal
+*/
+public class SensorValJsonSerde extends Serdes.WrapperSerde<SensorVal> {
+    
+    public SensorValJsonSerde() {
+        super(new Serializer<SensorVal>() {
+            private Gson gson = new Gson();
+
+            @Override
+            public void configure(Map<String, ?> map, boolean b) {
+            }
+
+            @Override
+            public byte[] serialize(String topic, SensorVal data) {
+                return gson.toJson(data);
+            } 
+
+            @Override
+            public void configure(Map<String, ?> configs, boolean isKey) {
+
+            }
+
+            @Override
+            public void close() {
+
+            }
+        },
+        new Deserializer<SensorVal>() {
+            private Gson gson = new Gson();
+
+            @Override
+            public void configure(Map<String, ?> configs, boolean isKey){
+
+            }
+
+            @Override
+            public SensorVal deserialize(String topic, byte[] data) {
+                return gson.fromJson(new String(data), SensorVal.class);
+            }
+
+            @Override
+            public void close() {
+
+            }
+        });
+    }
+}
